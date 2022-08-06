94a8fe4a94685669cc42761cf63ce4bd51dd53e1
bhosalenityanand <bhosalenityanand5@gmail.com>
2022-08-06 17:44:00 +0530

    Allure

---
+++ cypress/integration/Fail/login1.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe('verify the login for fail',function(){
+    it('verify with valid credential with fail',function(){
+        expect(1).to.eqls(2)
+    })
+})
